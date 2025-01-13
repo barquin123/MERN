24de110b50cca2779975c1953a291d88871fa6eb
JayFrank\FDC.JayFrank-NC-FE <fdc.jayfrankbarquin@gmail.com>
2025-01-13 10:51:58 +0800

    yeah

--- client/src/components/clientProvider/ClientProvider.jsx
+++ client/src/components/clientProvider/ClientProvider.jsx
@@ -1,13 +1,46 @@
 import { createContext } from "react";
-
+import axios from 'axios';
 const clientContext = createContext();
 
-export default clientProvider = ({children}) => {
+export const clientProvider = ({children}) => {
 
-    const tasks = 
+    const [tasks, setTasks] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [token, setToken] = useState('');
+    const [user, setUser] = useState('');
+
+    const fetchData = async () => {
+        try {
+            const response = await  axios.get('http://localhost:5000/tasks');
+            setTasks(response.data);
+            setLoading(false);
+        }catch (error) {
+            console.log(error);
+            setLoading(false);
+        }
+    };
+    fetchData();
+
+    const fetchUser = async(username, password) => {
+        try {
+            const response = await axios.get('http://localhost:5000/login',{
+                username,
+                password,
+            });
+            setToken(response.data.token);
+            setUser(username);
+            return {success: true, message: response.data.message};
+        } catch (error) {
+            return {success: false, message: error.response?.data?.error || 'An error occurred'};
+        }
+    }
+
+    fetchUser();
+
+
 
     return (
-        <clientContext.Provider>
+        <clientContext.Provider value = {{tasks, loading, token, user}}>
         {children}
         </clientContext.Provider>
     )
